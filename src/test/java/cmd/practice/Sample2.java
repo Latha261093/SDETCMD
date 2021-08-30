cba99ac1d380e4436167657887b9540382f91fe3
Latha231293 <lathaiem@gmail.com>
2021-08-30 12:40:42 +0530

    i have added new classes

---
+++ src/test/java/cmd/practice/Sample2.java
@@ -0,0 +1,12 @@
+package cmd.practice;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample 2 is created");
+
+
+	}
+
+}
